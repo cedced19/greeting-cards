8285ba5585df2df06f390d05a0f35b545e8defe9
Cédric <cedced19@gmail.com>
2015-10-09 11:00:50 +0200

    Delete google plus old page

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -5,16 +5,16 @@ angular.module('GreetingCards', ['ngCookies'])
             uri: 'http://cedced19.github.io/greeting-cards/cards.html#',
             sentence:'Merry christmas !'
         };
-        
-            
+
+
         $scope.compile = function(){
             $cookieStore.put('sentence', $scope.config.sentence);
             $scope.result = $scope.config.uri + $scope.config.sentence.replace(/ /g, '%20').replace(/ë/g, '%C3%AB').replace(/!/g, '%21');
-        }
-       
-        if($cookieStore.get('sentence') != undefined){
+        };
+
+        if($cookieStore.get('sentence') !== undefined){
             $scope.config.sentence = $cookieStore.get('sentence');
         }
-    
-        $scope.compile(); 
-}])
+
+        $scope.compile();
+}]);
